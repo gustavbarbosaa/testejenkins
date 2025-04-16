ca2f1df541baa94b53a0d43357486edf2fc875cd
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-04-15 21:11:58 -0300

    teste

--- src/main/java/br/edu/catolica/workshop/controller/HelloController.java
+++ src/main/java/br/edu/catolica/workshop/controller/HelloController.java
@@ -1,11 +1,11 @@
 package br.edu.catolica.workshop.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/hello")
 public class HelloController {
     @GetMapping("/{name}")
